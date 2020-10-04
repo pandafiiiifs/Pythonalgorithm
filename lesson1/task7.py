b231da0836e1a630707f3124348b483f083383ed
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2020-10-04 10:12:16 +0300

    test

---
+++ lesson1/task7.py
@@ -0,0 +1,34 @@
+"""
+Задание 5.
+Задание на закрепление навыков работы с деком
+
+В рассмотренном на уроке листинге есть один недостаток
+Приведенный код способен "обработать" только строку без пробелов, например, 'топот'
+
+Но могут быть и такие палиндромы, как 'молоко делили ледоколом'
+
+Вам нужно доработать программу так, чтобы она могла выполнить проверку на палиндром
+и в таких строках (включающих пробелы)
+
+"""
+from importfromlesson import DequeClass
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+
+    for el in string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first[::-1] != last: ##по идее вот тут должно быть сравнение слова с ним же наоборот
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("молоко делили ледоколом"))
